1d7ac58104775f03a994dfaeb6e3a0c03cdac2c2
Guilherme Marques ALEST <guilherme.camara@alest.com.br>
2022-10-11 13:22:51 -0300

    feat: Add styled-components and global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { GlobalStyle } from "./assets/styles/global";
+
+// import styled from "styled-components";
+// const Title = styled.h1`
+//   font-size: 64px;
+//   color: red;
+// `;
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
      <h1>Hello, World!</h1>
     </div>
   );
---
+++ src/assets/styles/global.ts
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root { //Variáveis do css para definir cores:
+    --red: #e52e4d;
+    --blue: #5429cc;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --shape: #ffffff;
+    --background: #f0f2f5;
+  }
+
+  //Tamanho de fonte da aplicação padrão vem como 16px, funciona muito bem para Desktop.
+  html {
+    @media (max-width: 1080px) { //Para uma tela até 1080px de largura o font-size será diminuido para 15px
+      font-size: 93.75%; //15px
+    }
+
+    @media (max-width: 720px) { //Para uma tela até 720px de largura o font-size será diminuido para 14px 
+      font-size: 87.5%; //14px
+    }
+  }
+  /* Por que diminuir a fonte? Primeiro que para dispositivos menores, faz senido ter uma fonte menor. 
+    Segundo: vamos utilizar daqui pra frente uma medida no CSS chamada REM, e 1REM = Tamanho do font-size da página. Dessa forma se o font-size é diminuido toda aplicação vai se diminuir tbm, vai adaptar a interface.
+    Terceiro: ao utilizar % , porque caso o usuário tenha alguma configuração no computador ou celular para mudar a fonte, o percentual vai adequar a preferẽncia do usuário.
+  */
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;  //fontes mais nítidas e detalhadas
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`;
